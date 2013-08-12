9b6fc16d4854087712ca28d6b3cd76c3d03e7208
Dmitry Davydov <haudvd@gmail.com>
2013-08-12 06:21:43 +0400

    simple first server

--- src/my_serv.erl
+++ src/my_serv.erl
@@ -27,18 +27,37 @@ start_link() ->
 %% ------------------------------------------------------------------
 
 init(Args) ->
-    {ok, Args}.
+	Opts = [{ip, {127,0,0,1}}],
+	case gen_tcp:listen(7654, Opts) of
+    {ok, Listen_socket} ->
+        gen_server:cast(?SERVER, loop),
+        {ok, Socket;
+    {error, Reason} ->
+        {stop, Reason}
+    end.
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
+handle_cast(loop, Socket) -> 
+	{ok, Conn} = gen_tcp:accept(Socket),
+	gen_server:cast(?SERVER, {send, Conn, "Hello Erlang!"}),
+	gen_server:cast(?SERVER, loop),
+	{noreply, Socket};
+handle_cast({send, Conn, Message}}, Socket) -> 
+	gen_tcp:send(Conn, Message),
+	gen_tcp:close(Conn),
+	{noreply, Socket};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+handle_info({inet_async, ListSock, Ref, Error) ->
+    {noreply, State};
 handle_info(_Info, State) ->
     {noreply, State}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, State = {Socket}) ->
+	gen_tcp:close(Socket)
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
